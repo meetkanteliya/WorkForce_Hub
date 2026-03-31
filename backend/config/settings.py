2555c23e6bf6a0130f1ae8e0f8ea3e1a309d0b0d
meetkanteliya <meetkanteliya@fxis.ai>
2026-03-31 15:40:36 +0530

    edit files.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/app/db/db.sqlite3',
     }
 }
 
